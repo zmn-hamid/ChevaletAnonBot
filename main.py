9ccc847f26c60b8b4566749806bc277bca799b6e
hamid <zmn-hamid@proton.me>
2024-08-31 19:20:08 +0330

    test flask

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 """main file"""
 
-# telegram imports
+# telegram & flask imports
 from telegram import *
 from telegram.ext import *
+
+from contextlib import asynccontextmanager
+from http import HTTPStatus
+from fastapi import FastAPI, Request, Response
 
 # project imports
 from config import BOT_TOKEN
@@ -26,47 +30,73 @@ from modules.other_msgs import other_messages_handler, other_cancels_handler
 from modules.app_handlers import error_handler, job_set_commands
 
 
-if __name__ == "__main__":
-    # build the application
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    job_queue = application.job_queue
 
-    # adding handlers
-    for handler in [
-        # start help
-        start_cmd_handler,
-        help_handler,
-        more_links_help_handler,
-        # mylinks
-        my_cids_handler,
-        rm_cid_handler,
-        add_cid_handler,
-        # settings
-        settings_handler,
-        settings_name_handler,
-        unblock_all_handler,
-        unblock_me_handler,
-        notify_src_handler,
-        notify_src_enable_handler,
-        notify_src_disable_handler,
-        # privacy
-        privacy_handler,
-        # admin
-        admin_handler,
-        # myuid
-        myuid_handler,
-        # other messages
-        other_cancels_handler,
-        other_messages_handler,
-    ]:
-        application.add_handler(handler)
+# build the application
+ptb = ApplicationBuilder().token(BOT_TOKEN).build()
+job_queue = ptb.job_queue
+bot = Bot(BOT_TOKEN)
 
-    # adding error handler
-    application.add_error_handler(error_handler)
 
-    # updating the bot's command menu
-    job_queue.run_once(job_set_commands, 3)
+# adding handlers
+for handler in [
+    # start help
+    start_cmd_handler,
+    help_handler,
+    more_links_help_handler,
+    # mylinks
+    my_cids_handler,
+    rm_cid_handler,
+    add_cid_handler,
+    # settings
+    settings_handler,
+    settings_name_handler,
+    unblock_all_handler,
+    unblock_me_handler,
+    notify_src_handler,
+    notify_src_enable_handler,
+    notify_src_disable_handler,
+    # privacy
+    privacy_handler,
+    # admin
+    admin_handler,
+    # myuid
+    myuid_handler,
+    # other messages
+    other_cancels_handler,
+    other_messages_handler,
+]:
+    ptb.add_handler(handler)
 
-    # starting the bot
-    logger.info("starting the bot...")
-    application.run_polling()
+# adding error handler
+ptb.add_error_handler(error_handler)
+
+# updating the bot's command menu
+job_queue.run_once(job_set_commands, 3)
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await bot.setWebhook('http://radioatur.com') # replace <your-webhook-url>
+    async with ptb:
+        await ptb.start()
+        yield
+        await ptb.stop()
+
+# Initialize FastAPI app (similar to Flask)
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/")
+async def process_update(request: Request):
+    req = await request.json()
+    update = Update.de_json(req, bot)
+    await ptb.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
+
+
+# WSGI application callable
+def application(environ, start_response):
+    return process_update(environ, start_response)
+
+
+# # starting the bot
+# logger.info("starting the bot...")
+# application.run_polling()
