c7347fbaa9f302b5b28652dd368407d20bfd9b7a
hamid <zmn-hamid@proton.me>
2024-08-31 20:27:03 +0330

    no flask again

--- main.py
+++ main.py
@@ -26,47 +26,46 @@ from modules.other_msgs import other_messages_handler, other_cancels_handler
 from modules.app_handlers import error_handler, job_set_commands
 
 
-if __name__ == "__main__":
-    # build the application
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    job_queue = application.job_queue
+# build the application
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+job_queue = application.job_queue
 
-    # adding handlers
-    for handler in [
-        # start help
-        start_cmd_handler,
-        help_handler,
-        more_links_help_handler,
-        # mylinks
-        my_cids_handler,
-        rm_cid_handler,
-        add_cid_handler,
-        # settings
-        settings_handler,
-        settings_name_handler,
-        unblock_all_handler,
-        unblock_me_handler,
-        notify_src_handler,
-        notify_src_enable_handler,
-        notify_src_disable_handler,
-        # privacy
-        privacy_handler,
-        # admin
-        admin_handler,
-        # myuid
-        myuid_handler,
-        # other messages
-        other_cancels_handler,
-        other_messages_handler,
-    ]:
-        application.add_handler(handler)
+# adding handlers
+for handler in [
+    # start help
+    start_cmd_handler,
+    help_handler,
+    more_links_help_handler,
+    # mylinks
+    my_cids_handler,
+    rm_cid_handler,
+    add_cid_handler,
+    # settings
+    settings_handler,
+    settings_name_handler,
+    unblock_all_handler,
+    unblock_me_handler,
+    notify_src_handler,
+    notify_src_enable_handler,
+    notify_src_disable_handler,
+    # privacy
+    privacy_handler,
+    # admin
+    admin_handler,
+    # myuid
+    myuid_handler,
+    # other messages
+    other_cancels_handler,
+    other_messages_handler,
+]:
+    application.add_handler(handler)
 
-    # adding error handler
-    application.add_error_handler(error_handler)
+# adding error handler
+application.add_error_handler(error_handler)
 
-    # updating the bot's command menu
-    job_queue.run_once(job_set_commands, 3)
+# updating the bot's command menu
+job_queue.run_once(job_set_commands, 3)
 
-    # starting the bot
-    logger.info("starting the bot...")
-    application.run_polling()
+# starting the bot
+logger.info("starting the bot...")
+application.run_polling()
